68352a50013863b331344544fbff01543b853364
fliegenhaan <perdanaraihan96@gmail.com>
2026-05-02 08:40:16 +0700

    fix: fix issue with db connection and relationship amongs data

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,22 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'username', 'user_type', 'is_active'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $connection = 'pgsql';
 
     /**
      * Get the attributes that should be cast.
@@ -27,6 +29,55 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
+    }
+
+    /**
+     * Get the student profile associated with the user.
+     */
+    public function student()
+    {
+        return $this->hasOne(Student::class);
+    }
+
+    /**
+     * Get the institution profile associated with the user.
+     */
+    public function institution()
+    {
+        return $this->hasOne(Institution::class);
+    }
+
+    /**
+     * Get the company profile associated with the user.
+     */
+    public function company()
+    {
+        return $this->hasOne(Company::class);
+    }
+
+    /**
+     * Check if user is a student.
+     */
+    public function isStudent(): bool
+    {
+        return $this->user_type === 'student';
+    }
+
+    /**
+     * Check if user is an institution.
+     */
+    public function isInstitution(): bool
+    {
+        return $this->user_type === 'institution';
+    }
+
+    /**
+     * Check if user is a company.
+     */
+    public function isCompany(): bool
+    {
+        return $this->user_type === 'company';
     }
 }
